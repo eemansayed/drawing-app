2c47ee232ec9476cbd0fd845171b60a20154774d
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-18 09:46:23 +0000

    add const called decrease

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("canvas");
-const Increase = document.getElementById("increase");
+const increase = document.getElementById("increase");
+const descrease = document.getElementById("decrease");
 const ctx = canvas.getContext("2d");
 
 let size = 20;
